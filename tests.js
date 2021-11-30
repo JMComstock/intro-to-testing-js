8654b4154a4064b8973c21dd35a20ddb3564a224
JMComstock <jasoncomstock11@gmail.com>
2021-11-30 15:55:57 -0600

    add the first test and solution for into-to-testing

--- tests.js
+++ tests.js
@@ -1,5 +1,5 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
+describe('helloWorld Unit Tests', function() {
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
     });
@@ -13,3 +13,29 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+
+describe('sayHello Unit Tests', function () {
+    it('should be a defined function', function() {
+        expect(sayHello).toBeDefined();
+    });
+});
+/*describe('sayHello Unit Tests' ,function() {
+    it('should be a defined function', function () {
+        expect(sayHello).toBeDefined();
+    });
+    it('should return a string', function () {
+        expect(sayHello()).toBe("string");
+    });
+    it('should return "Hello!" when no arguments are passed', function () {
+        expect(sayHello()).toBe("Hello!");
+    });
+    it('should return "Hello, Angel" when called with argument "Angel".', function () {
+        expect(sayHello("Angel")).toBe("Hello, Angel!");
+    });
+    it('should return "Hello, Andrew!" when called with argument "Andrew"', function () {
+        expect(sayHello()).toBe("Hello!")
+    });
+    it('Return "That\'s not a name!" when called with argument (number) 21.', function () {
+        expect(sayHello(21)).toBe("That's not a name!")
+    })
+});*/
